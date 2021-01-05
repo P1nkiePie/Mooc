b56ae6d49f967f43a03d8247a687d06439023bf4
P1nkiePie <70766855+P1nkiePie@users.noreply.github.com>
2021-01-05 18:47:51 +0200

    Add files via upload

---
+++ osa05-Osa05_04.Harjoitusapuri/src/main/java/Ohjelma.java
@@ -0,0 +1,17 @@
+
+public class Ohjelma {
+
+    public static void main(String[] args) {
+
+        Harjoitusapuri apuri = new Harjoitusapuri(30, 60);
+
+        double prosenttiosuus = 0.5;
+
+        while (prosenttiosuus < 1.0) {
+            double tavoite = apuri.tavoitesyke(prosenttiosuus);
+            System.out.println("Tavoite " + (prosenttiosuus * 100) + "% maksimista: " + tavoite);
+            prosenttiosuus += 0.1;
+        }
+
+    }
+}
