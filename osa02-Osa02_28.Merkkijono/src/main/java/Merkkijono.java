77c5eaaf50b3b86897b4f829c312626893d3ef6a
P1nkiePie <70766855+P1nkiePie@users.noreply.github.com>
2021-01-05 18:03:46 +0200

    Add files via upload

---
+++ osa02-Osa02_28.Merkkijono/src/main/java/Merkkijono.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Merkkijono {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        
+        merkkijono();
+
+    }
+    
+    public static String merkkijono() {
+        String eka = "Moikkelis";
+        return eka;
+    }
+
+}
