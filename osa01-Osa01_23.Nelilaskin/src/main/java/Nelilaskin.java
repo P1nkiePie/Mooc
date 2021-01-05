bf218b3619165146279ef6734b9e67ce623588f1
P1nkiePie <70766855+P1nkiePie@users.noreply.github.com>
2021-01-05 17:45:59 +0200

    Add files via upload

---
+++ osa01-Osa01_23.Nelilaskin/src/main/java/Nelilaskin.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Nelilaskin {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Syötä ensimmäinen luku!");
+        int eka = Integer.valueOf(lukija.nextLine());
+        System.out.println("Syötä toinen luku!");
+        int toka = Integer.valueOf(lukija.nextLine());
+        
+        System.out.println(eka + " + " + toka + " = " + (eka+toka));
+        System.out.println(eka + " - " + toka + " = " + (eka-toka));
+        System.out.println(eka + " * " + toka + " = " + (eka*toka));
+        double tulos = 1.0 * eka/toka;
+        System.out.println(eka + " / " + toka + " = " + tulos);
+
+    }
+}
