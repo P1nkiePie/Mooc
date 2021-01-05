1057132dcb5dbd4346471a18f6f6dc1015a3650d
P1nkiePie <70766855+P1nkiePie@users.noreply.github.com>
2021-01-05 18:24:23 +0200

    Add files via upload

---
+++ osa04-Osa04_01.EnsimmainenTilisi/src/main/java/EnsimmainenTilisi.java
@@ -0,0 +1,9 @@
+
+public class EnsimmainenTilisi {
+
+    public static void main(String[] args) {
+        Tili pekanTili = new Tili("Miron tili", 100.00);
+        pekanTili.pano(20);
+        System.out.println(pekanTili);
+    }
+}
