1b791275c2b62d5f89b622f94caf468b8e7565b4
P1nkiePie <70766855+P1nkiePie@users.noreply.github.com>
2021-01-05 18:30:28 +0200

    Add files via upload

---
+++ osa04-Osa04_13.Agentti/src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Agentti bond = new Agentti("James", "Bond");
+
+        bond.toString();
+        System.out.println(bond);
+
+        Agentti ionic = new Agentti("Ionic", "Bond");
+        System.out.println(ionic);
+
+    }
+}
