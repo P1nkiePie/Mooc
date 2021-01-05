e06c8f26e335e763c249a645c5306715ae5c334d
P1nkiePie <70766855+P1nkiePie@users.noreply.github.com>
2021-01-05 17:55:43 +0200

    Add files via upload

---
+++ osa02-Osa02_08.LukujenLukumaara/src/main/java/LukujenLukumaara.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class LukujenLukumaara {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        
+        int i = 0;
+        
+        while (true) {
+            System.out.println("Syötä luku");
+            int luku = Integer.valueOf(lukija.nextLine());
+            
+            if (luku == 0) {
+                break;
+            } else {
+                i++;
+            }
+        }
+        System.out.println("Lukuja yhteensä " + i);
+    }
+}
