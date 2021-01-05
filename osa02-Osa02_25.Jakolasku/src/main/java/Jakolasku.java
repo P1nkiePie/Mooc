0ea1132eb436994f0e030f13633f744c9b0354da
P1nkiePie <70766855+P1nkiePie@users.noreply.github.com>
2021-01-05 18:02:38 +0200

    Add files via upload

---
+++ osa02-Osa02_25.Jakolasku/src/main/java/Jakolasku.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Jakolasku {
+
+    public static void main(String[] args) {
+
+        jakolasku(3, 5);
+        
+    }
+    
+    public static void jakolasku(int osoittaja, int nimittaja) {
+        
+        double tulos = 1.0 * osoittaja/nimittaja;
+        System.out.println(tulos);
+        
+    }
+}
