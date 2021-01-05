d2e70e82f275d0b46413aed254896f8c9f85ca41
P1nkiePie <70766855+P1nkiePie@users.noreply.github.com>
2021-01-05 18:30:56 +0200

    Add files via upload

---
+++ osa04-Osa04_14.Kertoja/src/main/java/Kertoja.java
@@ -0,0 +1,13 @@
+public class Kertoja {
+    
+    private int luku;
+    
+    public Kertoja(int kertoja) {
+        this.luku = kertoja;
+    }
+    
+    public int kerro(int luku) {
+        return this.luku*luku;
+    }
+    
+}
---
+++ osa04-Osa04_14.Kertoja/src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+         Kertoja kolmellaKertoja = new Kertoja(3);
+         System.out.println(kolmellaKertoja.kerro(5));
+    }
+}
