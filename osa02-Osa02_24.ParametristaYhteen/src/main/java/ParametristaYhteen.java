14105b41aec8bbcd2f5feac8df5659987751a2ad
P1nkiePie <70766855+P1nkiePie@users.noreply.github.com>
2021-01-05 18:02:14 +0200

    Add files via upload

---
+++ osa02-Osa02_24.ParametristaYhteen/src/main/java/ParametristaYhteen.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class ParametristaYhteen {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        tulostaLuvustaYhteen(5);
+        
+    }
+    
+    public static void tulostaLuvustaYhteen(int luku) {
+        
+        int i = 1;
+        
+        while (luku > 0) {
+            System.out.println(luku);
+            luku--;
+        }
+        
+    }
+
+}
