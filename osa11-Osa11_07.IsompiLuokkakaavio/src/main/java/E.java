9e0e1ca96d285523505afc24c9090c026deca08a
Miro <70766855+P1nkiePie@users.noreply.github.com>
2021-05-04 18:04:04 +0300

    Add files via upload

---
+++ osa11-Osa11_07.IsompiLuokkakaavio/src/main/java/E.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rontt
+ */
+import java.util.*;
+public class E {
+    private ArrayList<C> lista;
+}
