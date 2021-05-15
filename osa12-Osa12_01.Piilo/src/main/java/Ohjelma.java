c2ad631a9d5c396e25332af61693c5c021c01f99
Miro <70766855+P1nkiePie@users.noreply.github.com>
2021-05-15 22:49:00 +0300

    Add files via upload

---
+++ osa12-Osa12_01.Piilo/src/main/java/Ohjelma.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Ohjelma {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        // voit testata luokkaasi täällä
+        Piilo<String> jemma = new Piilo<>();
+        System.out.println(jemma.onkoPiilossa());
+        jemma.laitaPiiloon("kukkuu");
+        System.out.println(jemma.onkoPiilossa());
+        System.out.println(jemma.otaPiilosta());
+        System.out.println(jemma.onkoPiilossa());
+        jemma.laitaPiiloon("kukkuluuruu");
+        jemma.laitaPiiloon("huhuu");
+        System.out.println(jemma.onkoPiilossa());
+        System.out.println(jemma.otaPiilosta());
+        System.out.println(jemma.onkoPiilossa());
+        
+        System.out.println("\n---\n");
+    
+        Piilo<Integer> piilo = new Piilo<>();
+        System.out.println(piilo.onkoPiilossa());
+        piilo.laitaPiiloon(1);
+        System.out.println(piilo.onkoPiilossa());
+        System.out.println(piilo.onkoPiilossa());
+        System.out.println(piilo.otaPiilosta());
+        System.out.println(piilo.onkoPiilossa());  
+    
+    }
+    
+}
---
+++ osa12-Osa12_01.Piilo/src/main/java/Piilo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rontt
+ */
+public class Piilo<T> {
+    private T piilo;
+    
+    public Piilo() {
+    }
+    
+    public void laitaPiiloon(T piilotettava) {
+        this.piilo = piilotettava;
+    }
+    
+    public T otaPiilosta() {
+        if(this.piilo==null) {
+            return null;
+        } else {
+            Object piilotettu = this.piilo;
+            this.piilo=null;
+            return (T) piilotettu;
+        }
+    }
+    
+    public boolean onkoPiilossa() {
+        if (this.piilo==null) return false;
+        return true;
+    }
+    
+}
